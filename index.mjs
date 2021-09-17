b5fe49b02f1caf8ad526659aceadc5eb31ac8b5a
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-17 17:42:07 -0300

    fix: package path to delete cache

--- index.mjs
+++ index.mjs
@@ -1,9 +1,21 @@
 import path from 'path';
+import url from 'url';
+
+import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-exports.get = (...args) => {
-  delete require.cache[path.join(process.cwd(), `node_modules`, `config`, `lib`, `config.js`)];
+const { log, error: consoleError } = console;
+
+const die = (...args) => {
+  consoleError(...args);
+  process.exit(1);
+};
+
+export const get = (...args) => {
+  delete require.cache[path.join(__dirname, `node_modules`, `config`, `lib`, `config.js`)];
+
   const config = require("config");
   return config.get(...args);
 }
